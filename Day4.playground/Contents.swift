fe73c9be06ef1c35643c0a20d9a5eb9520b4c90a
David Bochenski <david@goincremental.com>
2019-06-19 20:11:10 +0100

    Day 4

---
+++ Day4.playground/Contents.swift
@@ -0,0 +1,101 @@
+import UIKit
+
+var distanceFlown = 0
+
+let count = 1...10
+
+for number in count {
+    print("Number is \(number)")
+}
+
+let albums = ["Red", "1989", "Reputation"]
+
+for album in albums {
+    print("\(album) is on Apple Music")
+}
+
+print ("Players gonna ")
+
+for _ in 1...5 {
+    print("play")
+}
+
+var number = 1
+
+while number <= 20 {
+    print(number)
+    number += 1
+}
+
+print ("Ready or not, here I come!")
+
+number = 1
+
+repeat {
+    print(number)
+    number += 1
+} while number <= 20
+
+print ("Ready or not, here I come!")
+
+//will warn as unreachable code
+while false {
+    print("This is false")
+}
+
+repeat {
+    print("This is false")
+} while false
+
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    countDown -= 1
+}
+
+print("Blast off!")
+
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    
+    countDown -= 1
+}
+
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print ("\(i) * \(j) is \(product)")
+        
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+for i in 1...10 {
+    if i % 2 == 1 {
+        continue
+    }
+    
+    print(i)
+}
+
+
+var counter = 0
+
+while true {
+    print(" ")
+    counter += 1
+    
+    if counter == 273 {
+        break
+    }
+}
