12bbe29c8c26adc36dcd73a7ee87bbe8ce4ef18c
David Bochenski <david@goincremental.com>
2019-07-09 08:17:19 +0100

    Day 24

---
+++ Project04/Project04/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Project04
+//
+//  Created by David Bochenski on 09/07/2019.
+//  Copyright © 2019 David Bochenski. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://ctowho.codes")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open Page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+         ac.addAction(UIAlertAction(title: "ctowho.codes", style: .default, handler: openPage))
+         ac.addAction(UIAlertAction(title: "apple.com", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
+        webView.load(URLRequest(url: url))
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+}
+
