88dd4a0c2dccc35c2cd21c6ae45df8e4d51b444e
David Bochenski <david@goincremental.com>
2019-07-15 17:07:08 +0100

    Day 30

---
+++ Project06b/Project06b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project06b
+//
+//  Created by David Bochenski on 15/07/2019.
+//  Copyright © 2019 Go Incremental. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
