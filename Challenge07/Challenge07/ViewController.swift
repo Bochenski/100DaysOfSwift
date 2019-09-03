34503062766c7cfa9725a3f7e24581970754c8f4
David Bochenski <david.bochenski@cognitoiq.com>
2019-09-03 19:21:05 +0100

    Day 74

---
+++ Challenge07/Challenge07/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge07
+//
+//  Created by David Bochenski on 03/09/2019.
+//  Copyright © 2019 David Bochenski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
