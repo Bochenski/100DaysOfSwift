67319d7d7f24d3eba2cf03ae75d55d05501cdc8a
David Bochenski <david@goincremental.com>
2019-09-05 20:45:41 +0100

    Day 76

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var distanceReading: UILabel!
+    @IBOutlet var beaconName: UILabel!
     var locationManager: CLLocationManager?
+    var foundIt = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +36,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func startScanning() {
+        let uuid2 = UUID(uuidString: "74278BDA-B644-4520-8F0C-720EAF059935")!
+        scanFor(uuid2, 123, 457, "Beacon2")
         let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        scanFor(uuid, 123, 456, "Beacon1")
+    }
+    
+    func scanFor(_ uuid: UUID, _ major: CLBeaconMajorValue, _ minor: CLBeaconMinorValue, _ id: String) {
+        let bc = CLBeaconIdentityConstraint(uuid: uuid, major: major, minor: minor)
+        let br = CLBeaconRegion(beaconIdentityConstraint: bc, identifier: id)
         
-        locationManager?.startMonitoring(for: beaconRegion)
-        locationManager?.startRangingBeacons(in: beaconRegion)
+        locationManager?.startMonitoring(for: br)
+        locationManager?.startMonitoring(for: <#T##CLRegion#>)
+        locationManager?.startRangingBeacons(satisfying: bc)
+        
     }
     
     func update(distance: CLProximity) {
@@ -50,7 +62,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 self.view.backgroundColor = .orange
                 self.distanceReading.text = "NEAR"
             case .immediate:
-                self.view.backgroundColor = .gray
+                self.view.backgroundColor = .red
                 self.distanceReading.text = "RIGHT HERE"
             default:
                 self.view.backgroundColor = .gray
@@ -60,11 +72,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
     }
     
-    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if let beacon = beacons.first {
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+
+        for beacon in beacons {
+            if foundIt == false {
+                foundIt = true
+                let ac = UIAlertController(title: "Boom", message: "Beacon detected", preferredStyle: .alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                ac.addAction(action)
+                present(ac, animated: true)
+            }
+            
+            beaconName.text = "\(beaconConstraint.major ?? 0)-\(beaconConstraint.minor ?? 0)"
             update(distance: beacon.proximity)
-        } else {
-            update(distance: .unknown)
         }
     }
 }
